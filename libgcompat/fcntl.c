8a7e183ed3cf372563dc263066035870c3ea6995
Paper <paper@paper.us.eu.org>
2024-01-29 10:49:07 -0500

    add posix_fallocate64() wrapper
    Signed-off-by: Paper <paper@paper.us.eu.org>

---
+++ libgcompat/fcntl.c
@@ -0,0 +1,10 @@
+#include <fcntl.h>	/* posix_fallocate */
+
+typedef long long off64_t;
+
+/*
+ * posix_fallocate64: gnu-ified 64-bit posix_fallocate
+*/
+int posix_fallocate64(int fd, off64_t offset, off64_t len) {
+	return posix_fallocate(fd, offset, len);
+}
