27f24807866ee69b788bc364e809a387069f5599
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2024-11-11 19:11:28 +0100

    resolv: Implement res_nquery

--- libgcompat/resolv.c
+++ libgcompat/resolv.c
@@ -9,6 +9,7 @@
 #include <resolv.h> /* res_state */
 #include <stddef.h> /* NULL */
 #include <string.h> /* memcpy, memset */
+#include <stdlib.h>
 
 #include "alias.h" /* weak_alias */
 
@@ -52,3 +53,26 @@ int __dn_expand(const unsigned char *msg, const unsigned char *eomorig,
 {
     return dn_expand(msg, eomorig, comp_dn, exp_dn, length);
 }
+
+int __res_nquery(res_state statep, const char *dname, int class, int type,
+               unsigned char *answer, int anslen)
+{
+    int rc;
+    res_state tmp = malloc(sizeof(*statep));
+    if (!statep || statep == &_res) {
+        free(tmp);
+        return -1;
+    }
+
+    /* save ctx to tmp */
+    memcpy(tmp, &_res, sizeof(_res));
+    /* use the provided context */
+    memcpy(&_res, statep, sizeof(*statep));
+    rc = res_query(dname, class, type, answer, anslen);
+    /* restore everything */
+    memcpy(statep, &_res, sizeof(_res));
+    memcpy(&_res, tmp, sizeof(*tmp));
+    free(tmp);
+    return rc;
+}
+weak_alias(__res_nquery, res_nquery);
